08e16f5143e7236e86fa0dd4bf074b74efd17445
Hack Reactor Student <student@hackreactor.com>
2017-09-08 17:26:48 -0700

    save

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,14 +10,79 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
+var request = require('request');
 
 
-
-var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
+var fetchProfileAndWriteToFile = function (readFilePath, writeFilePath) {
   // TODO
+
+  var promise = new Promise((resolve, reject) => {
+    fs.readFile(readFilePath, 'utf8', (err, content) => {
+
+      if (err) {
+        reject(err);
+      } else {
+        var lines = content.split('\n');
+        var githubUser = 'https://api.github.com/users/' + lines[0];
+        request(githubUser, function (error, response, body) {
+          console.log(body);
+          if (error) {
+            reject(error);
+          } else {
+            // return body;
+            fs.writeFile(writeFilePath, body, (err) => {
+              if (err) {
+                reject(err);
+              } else {
+                resolve();
+              }
+            });
+          }
+        });
+      }
+    });
+  });
+
+  return promise;
 };
 
 // Export these functions so we can test them
 module.exports = {
   fetchProfileAndWriteToFile: fetchProfileAndWriteToFile
 };
+
+// var fs = require('fs');
+// var Promise = require('bluebird');
+// var pluckFirstLineFromFileAsync = require('./promiseConstructor').pluckFirstLineFromFileAsync;
+// var getGitHubProfileAsync = require('./promisification').getGitHubProfileAsync;
+
+
+// var fileWriter = function(writeFilePath, profile, callback) {
+//   fs.writeFile(writeFilePath, JSON.stringify(profile), 'utf8', function(err, file) {
+//     if (err) {
+//       return callback(err, null);
+//     }
+//     callback(null, profile);
+//   });
+// };
+
+// var fileWriterAsync = Promise.promisify(fileWriter);
+
+// var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
+
+//   return pluckFirstLineFromFileAsync(readFilePath)
+//     .then(function(userName){
+//       return getGitHubProfileAsync(userName);
+//     })
+//     .then(function(profile){
+//       return fileWriterAsync(writeFilePath, profile);
+//     })
+//     .catch(function(err){
+//     });
+
+// };
+
+// // Export these functions so we can test them
+// module.exports = {
+//   fetchProfileAndWriteToFile: fetchProfileAndWriteToFile
+// };
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -11,11 +11,33 @@ var Promise = require('bluebird');
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
   // TODO
+  var promise = new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, content) => {
+      // var lines = content.split('\n');
+      if (err) {
+        reject(err);
+      } else {
+        var lines = content.split('\n');
+        resolve(lines[0]);
+      }
+    });
+  });
+  return promise;
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
   // TODO
+  var promise = new Promise((resolve, reject) => {
+    request(url, (err, response, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(response.statusCode);
+      }
+    });
+  });
+  return promise;
 };
 
 // Export these functions so we can test them and reuse them in later exercises
